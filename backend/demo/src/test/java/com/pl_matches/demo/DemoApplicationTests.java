c924a6091c5748f3e28ba10d7b081f73fb3c18f4
saumG <saumster123@gmail.com>
2024-01-21 15:23:39 -0800

    initialized spring boot app for backend

---
+++ backend/demo/src/test/java/com/pl_matches/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl_matches.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
